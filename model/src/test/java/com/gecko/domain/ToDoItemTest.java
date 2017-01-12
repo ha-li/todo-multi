946a031df992dcab921e4ee6fe0c1f1ca3b541b6
ha-li <ha-li@gmail.com>
2017-01-11 22:33:55 -0800

    jacoco code coverage reporting

--- model/src/test/java/com/gecko/domain/ToDoItemTest.java
+++ model/src/test/java/com/gecko/domain/ToDoItemTest.java
@@ -1,7 +1,7 @@
 package com.gecko.domain;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Created by hlieu on 01/8/17.
@@ -16,9 +16,12 @@ public class ToDoItemTest {
       ToDoItem toDoItem = new ToDoItem ();
       toDoItem.setId(id);
       toDoItem.setName (name);
+      boolean complete = false;
+      toDoItem.setCompleted (complete);
 
       Assert.assertEquals(id, toDoItem.getId());
       Assert.assertEquals(name, toDoItem.getName ());
+      Assert.assertEquals(complete, toDoItem.isCompleted ());
    }
 
 }
